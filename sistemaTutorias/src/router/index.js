7c85ecd23d237d45ff6f26e0a333a533ad91cfb6
julifunes2010-bit <julifunes2010@gmail.com>
2026-09-03 09:19:34 -0300

    feat: add Gmail recovery route

--- sistemaTutorias/src/router/index.js
+++ sistemaTutorias/src/router/index.js
@@ -7,54 +7,20 @@ import register from "../register.vue"
 import reports from "../reports.vue"
 import recuperar from "../recuperar.vue"
 import cursos from "../cursos.vue"
+import EnviarGmail from "../Enviar-Gmail.vue"
 
-// para usar router pega este codigo en la terminal: npm install vue-router
 const router = createRouter({
-
   history: createWebHistory(),
-
   routes: [
-
-    {
-      path: "/setting",
-      name: "Setting",
-      component: Setting
-    },
-    {
-      path: "/",
-      name: "Login",
-      component: Login
-    },
-
-    {
-      path: "/home",
-      name: "Home",
-      component: Home
-    },
-    {
-      path: "/register",
-      name: "Register",
-      component: register
-    },
-    {
-      path: "/reportes",
-      name: "Reports",
-      component: reports
-    },
-    {
-      path: "/recuperar",
-      name: "Recuperar",
-      component: recuperar
-    },
-    {
-      path: "/cursos",
-      name: "Cursos",
-      component: cursos
-    }
-
+    { path: "/setting", name: "Setting", component: Setting },
+    { path: "/", name: "Login", component: Login },
+    { path: "/home", name: "Home", component: Home },
+    { path: "/register", name: "Register", component: register },
+    { path: "/reportes", name: "Reports", component: reports },
+    { path: "/recuperar", name: "Recuperar", component: recuperar },
+    { path: "/cursos", name: "Cursos", component: cursos },
+    { path: "/enviar-gmail", name: "EnviarGmail", component: EnviarGmail }
   ]
-
 })
-
 
 export default router
